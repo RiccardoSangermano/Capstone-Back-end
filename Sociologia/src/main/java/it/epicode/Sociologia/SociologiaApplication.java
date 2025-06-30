70053c2214e6e99a0ef766d45805fc44ce2008f4
RiccardoSangermano <riccardosangermano297@gmail.com>
2025-06-30 16:12:19 +0200

    start

---
+++ Sociologia/src/main/java/it/epicode/Sociologia/SociologiaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.Sociologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociologiaApplication.class, args);
+	}
+
+}
